dd5f02750ed926682fee4101d78adbb8a0f00696
Uncle Lin <unclelin2020@gmail.com>
2022-06-12 12:47:23 +0800

    feat: v-bind example

--- cli/src/views/Home.vue
+++ cli/src/views/Home.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="h-screen w-screen flex flex-col justify-center items-center">
-    <h1 class="text-5xl mb-3">v-model</h1>
-    <input
-      class="
-        border-2
-        rounded-lg
-        border-gray-800
-        focus:border-gray-700
-        outline-none
-        h-12
-        py-3
-        px-3
-      "
-      type="text"
-      placeholder="請輸入資料"
-      autofocus
-    />
+    <h1 class="text-5xl mb-3">v-bind</h1>
+    <div>
+      <input
+        class="
+          border-2
+          rounded-lg
+          border-gray-800
+          outline-none
+          h-12
+          py-3
+          px-3
+          focus:border-gray-700
+          disabled:border-0
+          disabled:text-gray-200
+          disabled:placeholder:text-gray-200
+        "
+        type="text"
+        placeholder="請輸入資料"
+        autofocus
+      />
+      <button
+        class="
+          ml-3
+          text-white
+          px-5
+          py-3
+          bg-gray-700
+          rounded-lg
+          shadow-md
+          hover:bg-gray-600
+        "
+      >
+        開啟
+      </button>
+    </div>
     <span class="text-lg mt-3">目前輸入的內容：{{ value || "尚未輸入" }}</span>
   </div>
 </template>
