4117a3b81b2f53e7e8beb499ff9176db6bac15ce
Uncle Lin <unclelin2020@gmail.com>
2022-06-12 20:28:04 +0800

    feat: v-if/v-show example

--- cli/src/views/Home.vue
+++ cli/src/views/Home.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="flex flex-col justify-center items-center h-screen">
+    <h1 class="text-4xl">v-if/v-show</h1>
+    <div class="flex gap-5 mt-4">
+      <div
+        class="
+          w-[300px]
+          h-[300px]
+          bg-gray-700
+          text-white
+          flex
+          justify-center
+          items-center
+          rounded-lg
+        "
+      >
+        v-if
+      </div>
+
+      <div
+        class="
+          w-[300px]
+          h-[300px]
+          bg-gray-700
+          text-white
+          flex
+          justify-center
+          items-center
+          rounded-lg
+        "
+      >
+        v-show
+      </div>
+    </div>
   </div>
 </template>
