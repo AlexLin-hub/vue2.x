1ef3b88c022349463c946f674bbf06c052ba55db
Uncle Lin <unclelin2020@gmail.com>
2022-06-12 12:34:08 +0800

    feat: v-on example

--- cli/src/views/Home.vue
+++ cli/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="h-screen w-screen flex flex-col justify-center items-center">
+    <h1 class="text-5xl mb-3">v-on</h1>
+    <button
+      class="
+        bg-gray-700
+        shadow-lg
+        text-white
+        py-3
+        px-5
+        rounded-lg
+        hover:bg-gray-500
+      "
+    >
+      點擊我
+    </button>
   </div>
 </template>
