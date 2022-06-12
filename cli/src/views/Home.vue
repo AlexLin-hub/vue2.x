11ee63741dc223e1d399e1d854b8904cea8f112d
Uncle Lin <unclelin2020@gmail.com>
2022-06-12 11:56:35 +0800

    feat: v-model example

--- cli/src/views/Home.vue
+++ cli/src/views/Home.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="h-screen w-screen flex flex-col justify-center items-center">
+    <h1 class="text-5xl mb-3">v-model</h1>
+    <input
+      class="
+        border-2
+        rounded-lg
+        border-gray-800
+        focus:border-gray-700
+        outline-none
+        h-12
+        py-3
+        px-3
+      "
+      type="text"
+      placeholder="請輸入資料"
+      autofocus
+    />
+    <span class="text-lg mt-3">目前輸入的內容：{{ value || "尚未輸入" }}</span>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+};
+</script>
