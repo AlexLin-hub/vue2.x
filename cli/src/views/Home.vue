ba5db20d59e2b1689d69d908a9de580830cf879d
Uncle Lin <unclelin2020@gmail.com>
2022-06-12 12:14:39 +0800

    feat: variable example

--- cli/src/views/Home.vue
+++ cli/src/views/Home.vue
@@ -1,5 +1,121 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="px-3">
+    <h1 class="text-3xl text-center mb-3">variable</h1>
+    <div class="grid grid-cols-4 gap-4">
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+      <div
+        class="
+          flex
+          justify-center
+          items-center
+          border-2 border-gray-800
+          rounded-lg
+          h-20
+        "
+      >
+        <span class="text-xl"></span>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      val01: "val01",
+      val02: "val02",
+      val03: "val03",
+      val04: "val04",
+      val05: "val05",
+      val06: "val06",
+      val07: "val07",
+      val08: "val08",
+      val09: "val09",
+      val10: "val10",
+    };
+  },
+};
+</script>
